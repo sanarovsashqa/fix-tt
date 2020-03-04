04b2287af1b45ff99a33317c02b3f9db95198073
Alexander Sanarov <ASanarov@stream.ru>
2020-03-04 12:33:46 +0300

    Updates

---
+++ precache-manifest.3cc78df5e60341f78d8df4574dbdc0fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efbfb5fed265f6f16b089bbf7f005ff",
+    "url": "/fix-tt/index.html"
+  },
+  {
+    "revision": "ff37a714b1ff99854b0a",
+    "url": "/fix-tt/static/css/main.1a90bee0.chunk.css"
+  },
+  {
+    "revision": "2000decb9c3f5dda8070",
+    "url": "/fix-tt/static/js/2.2a8bde07.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/fix-tt/static/js/2.2a8bde07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff37a714b1ff99854b0a",
+    "url": "/fix-tt/static/js/main.393547a4.chunk.js"
+  },
+  {
+    "revision": "31d1f06a580d64901eb7",
+    "url": "/fix-tt/static/js/runtime-main.182c37ad.js"
+  }
+]);
